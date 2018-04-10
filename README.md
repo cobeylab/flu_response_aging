ba45d39bc4f646c1925571f6b86b3258c5c2d107
Marcos Costa Vieira <mvieira@midway2-login2.rcc.local>
2018-04-10 16:44:39 -0500

    Adding details to README files

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # flu_response_aging
-Collaboration with Patrick's Wilson's group, led by Carole Henry: Aged individuals no longer efficiently adapt their B cell responses to influenza
+Collaboration with Patrick's Wilson's group, led by Carole Henry: Elderly individuals no longer adapt their B cell responses to influenza efficiently
 
+## Lineage assignment of influenza B sequences
+Script ```blast_B_to_reference_seqs.sbatch``` contains the commands used for blasting influenza B HA  sequences in ```genbank_B_HA.fasta``` against the reference strains in ```reference_B_strains.fasta``` (both files are in ```data/sequences```) 
+
+
+## 
--- src/combine_all_plots.R
+++ src/combine_all_plots.R
@@ -7,12 +7,20 @@ source('plot_divergence_vs_time_by_domain.R')
 
 # Adjust individual plots
 divergence_by_domain_plot <- divergence_by_domain_plot + 
-  theme(strip.background = element_blank(), strip.text = element_blank())
+  theme(strip.background = element_blank(), strip.text = element_blank(),
+        axis.title.y = element_text(size = 12), axis.text.y = element_text(size = 12),
+        axis.title.x = element_text(size = 12), strip.text = element_text(size = 12))
 
-glyc_vs_time_plot <- glyc_vs_time_plot + xlab('')
+divergence_vs_time_plot <- divergence_vs_time_plot + theme(axis.title.y = element_text(size = 12), 
+                                                           axis.text.y = element_text(size = 12),
+                                                           strip.text = element_text(size = 12))
+
+glyc_vs_time_plot <- glyc_vs_time_plot + xlab('') + theme(axis.title.y = element_text(size = 12), 
+                                                           axis.text.y = element_text(size = 12),
+                                                          strip.text = element_text(size = 12))
 
 final_plot <- plot_grid(divergence_vs_time_plot, glyc_vs_time_plot, divergence_by_domain_plot,
-          nrow = 3, labels = c('a','b','c'), label_y = c(1,1.1,1.1))
+          nrow = 3, labels = c('A','B','C'), label_y = c(1,1.1,1.1), label_size = 12)
 
 
 
